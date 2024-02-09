b5d554cde0d58ff45dc3035b6595ac2e8932ff35
Misha <mishaborunda7@gmail.com>
2024-02-09 14:51:35 -0600

    HW14

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,12 +18,56 @@ d3.json(url).then(function(data){
     //step3 -grab data for specific person
     let person = data.names[0];
     let person_data = data.samples.filter(row => row.id === person)[0];
-    console.log(person_data);
+    let meta_data = data.metadata.filter(row => row.id == person)[0];
+
+    console.log(meta_data);
 
     //make bar chart
     makeBar(person_data);
     makeBubble(person_data);
+    makeMeta(meta_data);
+    makeGauge(meta_data);
 });
+
+function makeMeta(meta_data) {
+    let panel = d3.select("#sample-metadata");
+    panel.html("");
+
+    //loop through each key
+    let keys = Object.keys(meta_data);
+    for (let i = 0; i< keys.length; i++){
+        let key = keys[i];
+        panel.append("p").text(`${key}: ${meta_data[key]}`);
+    }
+}
+
+function makeGauge(meta_data){
+    let trace1 = {
+        domain: { x: [0, 1], y: [0, 1] },
+        value: meta_data["wfreq"],
+        title: { text: "Belly Button Washing Frequency" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        delta: { reference: 5 },
+        gauge: {
+            axis: { range: [null, 10] },
+            steps: [
+                { range: [0, 5], color: "lightgray" },
+                { range: [5, 8], color: "gray" }
+            ],
+            threshold: {
+                line: { color: "red", width: 4 },
+                thickness: 0.75,
+                value: 9.5
+            }
+        }
+    }
+
+let traces = [trace1];
+let layout = {};
+Plotly.newPlot('gauge', traces, layout);
+
+}
 
 function makeBar(person_data){
         //step4 - given data make a bar chart
@@ -80,9 +124,13 @@ function makeBubble(person_data){
 
 function optionChanged(person){
     let person_data = global_data.samples.filter(row => row.id === person)[0];
+    let meta_data = global_data.metadata.filter(row => row.id == person)[0];
 
     //make bar chart
     makeBar(person_data);
+    makeBubble(person_data);
+    makeMeta(meta_data);
+    makeGauge(meta_data);
 }
 
 //populate the dropdown
